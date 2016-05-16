814d151babeeb52d2b2f0e382e31ab2e84576721
Simmy Pappachen <sspnyc@gmail.com>
2016-05-16 15:57:04 -0400

    new tweet bot

---
+++ twitterBot/bot.js
@@ -0,0 +1,5 @@
+console.log('The bot is starting');
+
+var Twit = require('twit');
+var config = require('./config');
+var T = new Twit(config);
---
+++ twitterBot/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'czvw8jYT63e5ZX3RW3tLnxnYI',
+  consumer_secret:      ' QE9iK75I9fbZoxZAaeIbpY3OGDNcCAuaDqOHcMncD9byi5wnVC',
+  access_token:         '11312162-zJMfvivFG75zLHrOVAKzUUBindbv3HfW6hpGGidVQ',
+  access_token_secret:  ' 0IjbFEtGsMYv29RmjJf0Chh6a0gtkHPHC6MLCAmK3CuQy',
+}
